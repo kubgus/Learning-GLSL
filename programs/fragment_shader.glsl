bd80ccb85832bb2e0331126ab1f262636e3dcb83
kubgus <ja.gustafik@outlook.sk>
2023-01-27 18:22:19 +0100

    DNA

--- programs/fragment_shader.glsl
+++ programs/fragment_shader.glsl
@@ -7,7 +7,7 @@ uniform float u_time;
 
 const float PI = acos(-1.0);
 
-float time = u_time * 0.01;
+float time = u_time;
 
 vec3 colorize(vec3 col, vec3 mod) {
     col.x *= mod.x;
@@ -22,10 +22,11 @@ void main() {
     vec3 col = vec3(0.0);
 
     float r = 0.3;
-    float l = 1000.0;
+    float l = 100.0;
     for (float i=0.0; i<l; i++) {
-        col += 0.0001 / length(vec2(uv.x - r * sin(time * i) * cos(PI/l*i*2), uv.y - r * cos(time * i) * sin(PI/l*i*2)));
+        col += 0.001 / length(vec2(uv.x - r * sin(PI/l*i*2 + time), uv.y - r * tan(PI/l*i*2)));
     }
 
-    fragColor = vec4(colorize(col, vec3(0.3, 0.3, 0.8)), 1.0);
+    fragColor = vec4(colorize(col, vec3(atan(time*2) + 0.1, 0, cos(-time*2) + 0.1)), 1.0);
+
 }
