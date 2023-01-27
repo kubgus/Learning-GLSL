3adc6c6db87f752edd1230eff401496a587aa4a2
kubgus <ja.gustafik@outlook.sk>
2023-01-27 16:38:06 +0100

    Sphere napriklad

--- programs/fragment_shader.glsl
+++ programs/fragment_shader.glsl
@@ -5,6 +5,8 @@ out vec4 fragColor;
 uniform vec2 u_resolution;
 uniform float u_time;
 
+const float PI = acos(-1.0);
+
 float time = u_time * 0.1;
 
 vec3 colorize(vec3 col, vec3 mod) {
@@ -19,10 +21,11 @@ void main() {
     vec2 uv = (gl_FragCoord.xy - 0.5 * u_resolution.xy) / u_resolution.y;
     vec3 col = vec3(0.0);
 
-    float l = 200.0;
+    float r = 0.3;
+    float l = 100.0;
     for (float i=0.0; i<l; i++) {
-        col += 0.001 / length(vec2(uv.x+0.8-(i/l)*1.61, uv.y + sin(time * i * 0.1) * 0.45));
+        col += 0.001 / length(vec2(uv.x - r * sin(time * i) * cos(PI/l*i*2), uv.y - r * sin(time * i) * sin(PI/l*i*2)));
     }
 
-    fragColor = vec4(colorize(col, vec3(0.2, 0.5, 0.8)), 1.0);
+    fragColor = vec4(colorize(col, vec3(0.5, 0.1, 0.8)), 1.0);
 }
